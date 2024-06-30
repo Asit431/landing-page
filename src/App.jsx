bd7f9565aaf202474362aa28c4e12af7900044b8
Asit431 <asitparia314@gmail.com>
2024-06-30 17:15:26 +0530

    development of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 // import './App.css'
+
+import Hero from './component/Hero'
 import Navbar from './component/Navbar'
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
